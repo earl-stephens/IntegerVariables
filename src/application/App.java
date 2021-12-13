67dad7e4312d67c05a4d8cc0d7e262887bd0da89
earl-stephens <stephensearl@hotmail.com>
2021-12-12 23:09:01 -0700

    Added several int variables

--- src/application/App.java
+++ src/application/App.java
@@ -4,11 +4,14 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		int cats;
-		cats = 7;
+		int cats = 12;
+		int dogs = 5;
+		int birds = 6;
 		
-		System.out.println("How many cats are there?");
-		System.out.println(cats);
+		int total = cats + birds + dogs;
+		
+		System.out.println("How many animals are there?");
+		System.out.println(total);
 
 	}
 
